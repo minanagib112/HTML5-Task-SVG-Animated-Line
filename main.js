6d4d16dc8f40bfccb730f0b91b67c39a4ea2435d
Mina Naguib <132841396+minanagib112@users.noreply.github.com>
2024-01-08 16:17:55 +0200

    Adding Files

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-function changeColor() {
-  var red = document.getElementById("red");
-  var green = document.getElementById("green");
-  var blue = document.getElementById("blue");
-  var text = document.getElementById("text");
+var line = document.getElementById("myLine");
+var x = 0 ;
+var y = 0;
 
-  var r = parseInt(red.value);
-  var g = parseInt(green.value);
-  var b = parseInt(blue.value);
-
-  var hexR = r.toString(16);
-  var hexG = g.toString(16);
-  var hexB = b.toString(16);
-
-  if (hexR.length == 1) hexR = "0" + hexR;
-  if (hexG.length == 1) hexG = "0" + hexG;
-  if (hexB.length == 1) hexB = "0" + hexB;
-
-  var color = "#" + hexR + hexG + hexB;
-
-  text.style.color = color;
+function animateLine() {
+  x += 5; 
+  y += 5; 
+  line.setAttribute("x2", x); 
+  line.setAttribute("y2", y); 
+  if (x == 305 && y == 305) { 
+    clearInterval(interval);
+    alert("animation end!");
+  
 }
-
-red.addEventListener("input", changeColor);
-green.addEventListener("input", changeColor);
-blue.addEventListener("input", changeColor);
+}
+var interval = setInterval(animateLine, 100); 
